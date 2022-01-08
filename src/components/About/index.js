520e89518effdb882add21de366f1ed5e24fe18c
grsmith35 <gordonrileysmith@gmail.com>
2022-01-08 16:53:16 -0700

    final touches

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,9 +7,9 @@ function About() {
         <div className="container columns m-3">
             <div className="column is-5 aboutme">
                 <h3>A bit about Me!</h3>
-                <div>I am a hard working, dedicated, and motivated person! I am always pushing to become better in what ever it is i am doing. I never want to settle for mediocrity!</div>
-                <div>I have worked in IT for the last 6 years at a staffing agency. While working there I would see people waste time on repetitive tasks and i looked for ways to automate those tasks. This started my journey with coding. I started writing python scripts for people to use to help convert spreadsheets, send text messages, and create job templates. All the other tasks in my job became boring and I was always looking for new coding projects. I found the U of U full stack boot camp in May of 2021 and decided to make the change into my new career.</div>
-                <div>I love being with people I care about. What ever that includes I'm game for. From sitting around the table playing games or just chatting to going camping or mountain biking, I'm in if it's with someone important in my life.</div>
+                <div>I am a hard working, dedicated, and motivated person! I am always pushing to become better in whatever it is I am doing. I never want to settle for mediocrity!</div>
+                <div>I have worked in IT for the last 6 years at a staffing agency. While working there I would see people waste time on repetitive tasks and looked for ways to automate those tasks. This started my journey with coding. I started writing python scripts for people to use to help convert spreadsheets, send text messages, and create job templates. All the other tasks in my job became boring and I was always looking for new coding projects. I found the University of Utah Full Stack Bootcamp in May of 2021 and decided to shitf my career in this new direction.</div>
+                <div>I love being with people I care about, whatever that includes; from playing board games, to getting outdoors for something adventurous.</div>
                 
             </div>
             <div className="column is-5">
